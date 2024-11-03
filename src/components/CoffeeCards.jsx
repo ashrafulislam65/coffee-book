e305f7da2054ca02a134f3d0c14b57e8c2c322af
ashrafulislam65 <md.ashraful.islam7758a@gmail.com>
2024-11-03 23:04:59 +0600

    added tab section

--- src/components/CoffeeCards.jsx
+++ src/components/CoffeeCards.jsx
@@ -1,16 +1,27 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Card from "./Card";
+import { useEffect, useState } from "react";
 
 const CoffeeCards = () => {
     const data = useLoaderData()
-    const {category} = useParams()
+    const { category } = useParams()
+    const [coffees, setCoffees] = useState([])
+    useEffect(() => {
+        if (category) {
+            const filteredByCategory = [...data].filter(coffee => coffee.category === category)
+            setCoffees(filteredByCategory)
+        }
+        else{
+            setCoffees(data.slice(0,6))
+        }
+    }, [category, data])
     console.log(data);
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {
-                data.map(coffee =>(
+                coffees.map(coffee => (
                     <Card key={coffee.id} coffee={coffee}></Card>
-                
+
                 ))
             }
         </div>
